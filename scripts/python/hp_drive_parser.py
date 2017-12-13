072fb8251a970532265ab5f4d1a61ca4ca3fbbe7
Jonathon Pinnow <pinnowj@gmail.com>
2017-12-13 13:09:41 -0500

    Add list process function and error handling
    The new process function helps glue together the many utility functions
    that produce the desired data structure.
    
    Did some error handling in case I need to change some of the default
    arguments. This is in hopes of reusing the functions for parsing
    the other output files I have left to convert.
    
    The diff will look kind of crazy because I reorganized the code a bit.

--- scripts/python/hp_drive_parser.py
+++ scripts/python/hp_drive_parser.py
@@ -1,5 +1,6 @@
 #!/bin/usr python3
 
+# ----- SETUP VARIABLES ------ #
 FILE_PATH = '..\\..\\sample_info\\server-specs\\USE014NF5K-spec'
 FILE = 'hp-drives.txt'
 TERM = 'drive'
@@ -17,22 +18,8 @@ with open(FILE_PATH + '\\' + FILE, 'r') as f:
 
 file_lines = drive_file.split('\n')
 
-
-def term_index(term, lines):
-    '''
-    Small function that takes a list and a string and returns a list of line
-    indexes where that term shows up
-    '''
-    result = list()
-
-    for index, line in enumerate(lines):
-        if term in line:
-            result.append(index)
-
-    return result
-
-
-def get_context(term,lines):
+# ------ BEGIN FUNCTIONS -------- #
+def get_context(lines, term=TERM):
     '''
     Get the end index for each start index in a list
 
@@ -51,6 +38,46 @@ def get_context(term,lines):
         end_index.append(start_index[index+1] -1 )
 
     return sublist(context,lines)
+
+
+def process_drive_list(drive_list):
+    '''
+    Takes a list of list and returns a list of dicts
+
+    Using a lot of the utility functions this, function processes
+    sub lists into easier to use dictionaries for later usage
+
+    One of those intended uses is converting to json and then sending to a 
+    RESTful API
+
+    example:
+    process_drive_list([
+        ['spec1: value1', '   spec2: value2'],
+        ['spec1: value1', '   spec2: value2']
+    ]) 
+    > [
+        {'spec1': 'value1', 'spec2': 'value2'},
+        {'spec1': 'value1', 'spec2': 'value2'}
+        ]
+    '''
+    try:
+        return [get_hp_drive_attributes(lines_to_dict(drive)) for drive in drive_list]
+    except:
+        print("Could not process drive list")
+
+# ------- UTILITY FUNCTIONS ------- #
+def term_index(term, lines):
+    '''
+    Small function that takes a list and a string and returns a list of line
+    indexes where that term shows up
+    '''
+    result = list()
+
+    for index, line in enumerate(lines):
+        if term in line:
+            result.append(index)
+
+    return result
 
 
 def sublist(bounds, lines):
@@ -73,6 +100,12 @@ def lines_to_dict(lines):
     lines_to_dict(file_lines)
     > {'physicaldrive': 1, 'SerialNumber': '12345hgfd'}
     '''
+    if type(lines).__name__ != 'list':
+        raise TypeError(
+            "Argument needs to be of <class list>, "
+            "<class {}> was given instead".format(type(lines).__name__)
+            ,lines)
+
     result = dict()
 
     for line in lines:
@@ -83,5 +116,14 @@ def lines_to_dict(lines):
     return result
 
 
-def get_hp_drive_attributes(attribs, dict_):
-    return { key: dict_[key] for key in attribs }
+def get_hp_drive_attributes(dict_, attribs=HP_DRIVE_ATTRIBS):
+    '''
+    Take a dictionary of HP drive specs and return a new dictionary of specs 
+    based on a given list
+    '''
+    try:
+        assert type(dict_).__name__ == 'dict', "Need to make sure to pass in a dict"
+        return { key: dict_[key] for key in attribs }
+    except KeyError:
+        print("Key value does not exist in your list or dict argument")
+
