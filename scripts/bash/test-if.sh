734d0172b5c792f69955a0abb2b6fbee16a3323a
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-03 11:05:54 -0500

    Remove if construct and Add for loop to setup interfaces
    No longer checking for interfaces that are setup already
    using ifconfig
    
    Added a for loop to setup each of the captured interfaces in the INET
    variable
    
    Changed the way the NFS mount is checked and setup
    
    Added some commenting to help make the script more clear on what's
    happening along the way

--- scripts/bash/test-if.sh
+++ scripts/bash/test-if.sh
@@ -4,31 +4,39 @@ INET=`ifconfig -a | sed -n 's/^\S/&/p' | sed -n 's/^e.*/&/p' | cut -d ' ' -f 1`
 ETHCONF=/etc/network/interfaces
 NFS="10.11.203.100:/nfs"
 
-if [ -z "$INET" ]
-then
-    echo "Setting up interface..."
-    INET=`ifconfig -a | cut -d ' ' -f 1 | sed -n -e '1p'`
-    echo "source /etc/network/interfaces.d*" > $ETHCONF
-    echo "auto lo" >> $ETHCONF
-    echo "iface lo inet loopback" >> $ETHCONF
-    echo "" >> $ETHCONF
-    echo "auto $INET" >> $ETHCONF
-    echo "iface $INET inet dhcp" >> $ETHCONF
-    echo "" >> $ETHCONF
+# Initialize interface file
+echo "Setting up interface..."
+echo "source /etc/network/interfaces.d*" > $ETHCONF
+echo "auto lo" >> $ETHCONF
+echo "iface lo inet loopback" >> $ETHCONF
+echo "" >> $ETHCONF
 
-    dhclient $INET
-    echo "Interface $INET is now setup"
+# Build out interface file config
+for i in $INET
+do
+    echo "auto $i" >> $ETHCONF
+    echo "iface $i inet dhcp" >> $ETHCONF
+    echo "" >> $ETHCONF
+done
+
+# Bring up each interface
+for i in $INET
+do
+    dhclient $i
+    echo "Interface $i is now setup"
+done
+
+MOUNT=`df -h | grep -i /mnt`
+if [ -z "$MOUNT" ]
+then
+    echo "Mounting NFS from $NFS ..."
     mount $NFS /mnt
-else
-    echo 'Interface is up'
+
+    # Check if mounted after mount attempt
     MOUNT=`df -h | grep -i /mnt`
-    if [ -z "$MOUNT" ]
-    then
-        echo "Mounting NFS from $NFS ..."
-        mount 10.11.203.100:/nfs /mnt
-        echo "Mount complete"
-    else
-        echo "NFS mounted from $NFS"
-    fi
+    [ ! -z "$MOUNT"] && echo "Mount from $NFS complete" || echo "FAILED Mount from $NFS"
+
+else
+    echo "NFS already mounted from $NFS"
 fi
 
