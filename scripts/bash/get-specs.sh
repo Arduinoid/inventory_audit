e6ae59a6b615f6cece8cdce725239f26406ef5f5
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-05 16:16:09 -0500

    Split up audit into individual part specific files for easier parsing

--- scripts/bash/get-specs.sh
+++ scripts/bash/get-specs.sh
@@ -17,6 +17,12 @@ dmidecode -t 1 | sed -e 's/\t//g' -e 's/\s//g' | sed -n -e '/Manufacturer/p' -e 
 dmidecode -t 3 | sed -e 's/\t//g' -e 's/\s//g' | sed -n '/Asset/p' >> $DIRPATH/dmi-system.txt
 dmidecode -t 17 | sed -e 's/\t//g' -e 's/\s//g' | sed -n -e '/Manufacturer/p' -e '/Part/p' -e '/Serial/p' -e '/Size/p' -e'/Type/p' -e '/Rank/p' > $DIRPATH/dmi-memory.txt
 lshw -json -quiet > $DIRPATH/lshw-report.json
+lshw -html -quiet > $DIRPATH/lshw-report.html
+lshw -businfo -quiet > $DIRPATH/lshw-businfo.json
+lshw -json -class processor -quiet > $DIRPATH/lshw-processor.json
+lshw -json -class memory -quiet > $DIRPATH/lshw-memory.json
+lshw -json -class disk -quiet > $DIRPATH/lshw-disk.json
+lshw -json -class network -quiet > $DIRPATH/lshw-network.json
 hpdiscovery -f $DIRPATH/hpdiscovery-report.xml
 lspci -nn | grep -i net > $DIRPATH/lspci-net.txt
 
@@ -30,7 +36,7 @@ case "$MANU" in
     MegaCli64 -PDList -aALL > $DIRPATH/dell-drives.txt
     ;;
 *)
-    lshw -C disk -quiet -json > $DIRPATH/server-drives.json
+    lshw -class disk -class storage -quiet -json > $DIRPATH/server-drives.json
     ;;
 esac
 
