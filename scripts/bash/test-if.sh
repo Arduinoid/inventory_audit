899807d5616ada3eb9add5296b9b610889cb1612
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-08 17:06:48 -0500

    Make test-if script more simple to get working

--- scripts/bash/test-if.sh
+++ scripts/bash/test-if.sh
@@ -12,25 +12,12 @@ echo "iface lo inet loopback" >> $ETHCONF
 echo "" >> $ETHCONF
 
 # Build out interface file config
+echo "Creating config file"
 for i in $INET
 do
     echo "auto $i" >> $ETHCONF
     echo "iface $i inet dhcp" >> $ETHCONF
     echo "" >> $ETHCONF
-    UPLINK=`cat /sys/class/net/$i/operstate`
-    if [ "$UPLINK" = "up" ] || [ "$UPLINK" = "unknown" ]
-    then
-        dhclient $i
-        IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' '  -f 2`
-        if [ -z "$IPADDR" ]
-        then
-            dhclient $i
-            IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' '  -f 2`
-        fi
-        [ ! -z $IPADDR ] && echo "Interface $i is now setup with ip: $IPADDR"; exit 0 || echo "FAILED to setup interface $i"; ((ECODE++))
-    else
-        echo "interface $i is not connected"
-        ((ECODE++))
-    fi
 done
-exit $ECODE
+echo "Obtaining IP address from DHCP server"
+dhclient $INET
