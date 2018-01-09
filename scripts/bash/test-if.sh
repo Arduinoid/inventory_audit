d98551ae8fcd9081a1e4a546c97b7e3f1d76e16a
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-09 12:50:43 -0500

    Add exit code to test script and Add exit check to rc.local file

--- scripts/bash/test-if.sh
+++ scripts/bash/test-if.sh
@@ -20,6 +20,8 @@ do
     echo "" >> $ETHCONF
     echo "Obtaining IP address from DHCP server"
     dhclient $i
+    sleep 1
     IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' ' -f 2`
-    [ ! -z "$IPADDR" ] && echo "Interface $i has ip: $IPADDR" || echo "Couldn't assign interface $i an ip"
+    [ ! -z "$IPADDR" ] && echo "Interface $i has ip: $IPADDR"; exit 0 || echo "Couldn't assign interface $i an ip"; ((ECODE++))
 done
+exit $ECODE
