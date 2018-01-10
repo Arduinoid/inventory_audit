a24205b32ec4ab5689b20da592b4287b8f7e1833
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-10 09:51:48 -0500

    Add a main entry point script and rename parser script so it can be imported

---
+++ scripts/python/main.py
@@ -0,0 +1,9 @@
+from parser_utils import *
+from utils import *
+
+FILE_PATH = "//10.11.203.100/nfs/server-spec"
+
+if __name__ == "__main__":
+    print("//========================\\")
+    print("||    Script running...    ||")
+    print("\\========================//")
