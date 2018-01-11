617c7009b078d2108e95be4ac47bec5c0f37e124
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-11 10:49:30 -0500

    Change how interfaces are brought up and Make function to check for IP's

--- scripts/bash/test-if.sh
+++ scripts/bash/test-if.sh
@@ -4,6 +4,14 @@ INET=`ifconfig -a | sed -n 's/^\S/&/p' | sed -n 's/^e.*/&/p' | cut -d ' ' -f 1`
 ETHCONF=/etc/network/interfaces
 ECODE=0
 
+check_ips() {
+    for i in $INET
+    do
+        IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' ' -f 2`
+        [ ! -z "$IPADDR" ] && echo "Interface $i has ip: $IPADDR"; exit 0 || echo "Couldn't assign interface $i an ip"; ((ECODE++))
+    done
+}
+
 # Initialize interface file
 echo "Setting up interface..."
 echo "source /etc/network/interfaces.d*" > $ETHCONF
@@ -19,9 +27,20 @@ do
     echo "iface $i inet dhcp" >> $ETHCONF
     echo "" >> $ETHCONF
     echo "Obtaining IP address from DHCP server"
-    dhclient $i
-    sleep 1
-    IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' ' -f 2`
-    [ ! -z "$IPADDR" ] && echo "Interface $i has ip: $IPADDR"; exit 0 || echo "Couldn't assign interface $i an ip"; ((ECODE++))
+    # dhclient $i
+    # sleep 1
+    # IPADDR=`ip addr show $i | grep -i inet | sed -n 's/\s*//p' | cut -d ' ' -f 2`
+    # [ ! -z "$IPADDR" ] && echo "Interface $i has ip: $IPADDR"; exit 0 || echo "Couldn't assign interface $i an ip"; ((ECODE++))
 done
+
+/etc/init.d/networking restart
+ifup -a
+check_ips
+
+if [ $ECODE -gt 0 ]
+then
+    ECODE=0
+    ifup -a
+    check_ips
+fi
 exit $ECODE
