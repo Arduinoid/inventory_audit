500a340b0025e2a9f82d7ccd34947b8a2bb387d3
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-11 11:20:45 -0500

    Change the way server connection tests are done before mount and don't block if not able to mount

--- scripts/bash/check-mount.sh
+++ scripts/bash/check-mount.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SERVER="10.11.203.100:/nfs"
+SERVER="10.11.203.100"
 MOUNT=`df | grep -i nfs`
 COUNT=1
+PCOUNT=0
 
-while [ -z "$MOUNT" ]
+ping -c 1 $SERVER > /dev/null
+echo "Attempting to reach server at $SERVER"
+while [ $? -ne 0 ]
 do
-    echo "Attempting mount try number: $COUNT"
-    mount $SERVER /mnt
-    ((COUNT++))
-    sleep 2
-    MOUNT=`df | grep -i nfs`
+    ((PCOUNT++))
+    ping -c 1 $SERVER > /dev/null
+    [ $PCOUNT -eq 4 ] && echo "Could not reach NFS server at $SERVER"; exit $PCOUNT
 done
 
-if [ ! -z "$MOUNT" ]
-then
-    echo "NFS mounted"
-    echo $MOUNT
-fi
+echo "Attempting mount..."
+mount $SERVER:/nfs /mnt
+MOUNT=`df | grep -i nfs`
+[ ! -z "$MOUNT" ] && echo "NFS mounted"; exit 0 || echo "Failed to mount NFS"; exit 1
