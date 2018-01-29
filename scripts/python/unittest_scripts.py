42cf8c48ac391117bd1b2d810dda135ba5060d88
Jonathon Pinnow <pinnowj@gmail.com>
2018-01-29 16:12:17 -0500

    Change tests to reflect the expected refactor

--- scripts/python/unittest_scripts.py
+++ scripts/python/unittest_scripts.py
@@ -7,32 +7,32 @@ FILES = os.listdir(FILE_PATH)
 
 class ParserTests(unittest.TestCase):
     def test_chassis_parser_returns_dict_when_called(self):
-        obj = ChassisParser(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),dict)
+        obj = ChassisParser(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),dict)
     
     def test_cpu_parser_returns_list_when_called(self):
-        obj = CPUParser(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),list)
+        obj = CPUParser(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),list)
     
     def test_drive_parser_returns_list_when_called(self):
-        obj = DriveParser(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),list)
+        obj = DriveParser(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),list)
     
     def test_memory_parser_returns_list_when_called(self):
-        obj = MemoryParser(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),list)
+        obj = MemoryParser(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),list)
     
     def test_mac_parser_returns_dict_when_called(self):
-        obj = MacAddressParse(FILE_PATH, 'SFP')
-        self.assertIsInstance(obj('HPCardFiberCardTest-spec'),dict)
+        obj = MacAddressParse(FILE_PATH, 'HPCardFiberCardTest-spec', 'SFP')
+        self.assertIsInstance(obj(),dict)
     
     def test_network_parser_returns_list_when_called(self):
-        obj = NetworkParser(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),list)
+        obj = NetworkParser(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),list)
     
     def test_server_parser_returns_dict_when_called(self):
-        obj = ServerParse(FILE_PATH)
-        self.assertIsInstance(obj(FILES[0]),dict)
+        obj = ServerParse(FILE_PATH, FILES[0])
+        self.assertIsInstance(obj(),dict)
     
     
 if __name__ == '__main__':
